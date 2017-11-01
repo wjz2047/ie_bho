bfab15b9f01085517896e60d4d230256d3ce65e0
weijiazhen <wjz2047@163.com>
2017-11-01 13:39:12 +0800

    修改BHO类

--- SevenFanli/SevenFanliBHO.cpp
+++ SevenFanli/SevenFanliBHO.cpp
@@ -6,3 +6,89 @@
 
 // CSevenFanliBHO
 
+CSevenFanliBHO::CSevenFanliBHO()
+{
+	m_isRefresh = TRUE;
+}
+
+STDMETHODIMP CSevenFanliBHO::SetSite(IUnknown* pUnkSite)  
+{  
+	if (pUnkSite != NULL)  
+	{  
+		// Cache the pointer to IWebBrowser2.  
+		HRESULT hr = pUnkSite->QueryInterface(IID_IWebBrowser2, (void **)&m_spWebBrowser);  
+		if (SUCCEEDED(hr))  
+		{  
+			// Register to sink events from DWebBrowserEvents2.  
+			hr = DispEventAdvise(m_spWebBrowser);  
+			if (SUCCEEDED(hr))  
+			{  
+				m_fAdvised = TRUE;  
+			}  
+		}  
+	}  
+	else  
+	{  
+		// Unregister event sink.  
+		if (m_fAdvised)  
+		{  
+			DispEventUnadvise(m_spWebBrowser);  
+			m_fAdvised = FALSE;  
+		}  
+
+		// Release cached pointers and other resources here.  
+		m_spWebBrowser.Release();  
+	}  
+
+	// Call base class implementation.  
+	return IObjectWithSiteImpl<CSevenFanliBHO>::SetSite(pUnkSite);  
+}  
+
+void STDMETHODCALLTYPE CSevenFanliBHO::OnBeforeNavigate2(IDispatch* pDisp, VARIANT* URL, VARIANT* pvtFlags, VARIANT* pvtTargetFrameName, 
+	VARIANT* pvtPostData, VARIANT* pvtHeaders, VARIANT_BOOL* pvbCancel)
+{
+	m_isRefresh = FALSE;
+}
+
+void STDMETHODCALLTYPE CSevenFanliBHO::OnDocumentComplete(IDispatch *pDisp, VARIANT *pvarURL)  
+{  
+	CComQIPtr<IWebBrowser2> spTempWebBrowser = pDisp;
+	if (spTempWebBrowser && m_spWebBrowser && m_spWebBrowser.IsEqualObject(spTempWebBrowser)) {
+		m_isRefresh = TRUE;
+
+		CComPtr<IDispatch> spDispDoc;
+		HRESULT hr = m_spWebBrowser->get_Document(&spDispDoc);
+		if (SUCCEEDED(hr)) { 
+			CComQIPtr<IHTMLDocument2> spHTMLDoc = spDispDoc;
+			if (spHTMLDoc != NULL) {
+				IHTMLWindow2 *spWindow;
+				if (SUCCEEDED(spHTMLDoc->get_parentWindow(&spWindow))) {
+					VARIANT vEmpty = {0};
+					CComBSTR js_code = L"alert(111)";
+					CComBSTR bstrLanguage = L"javascript";
+					spWindow->execScript(js_code, bstrLanguage, &vEmpty);
+				}
+			}
+		}
+	} 
+}  
+
+void STDMETHODCALLTYPE CSevenFanliBHO::OnDownloadComplete()
+{
+	if (m_isRefresh) {
+		CComPtr<IDispatch> spDispDoc;
+		HRESULT hr = m_spWebBrowser->get_Document(&spDispDoc);
+		if (SUCCEEDED(hr)) { 
+			CComQIPtr<IHTMLDocument2> spHTMLDoc = spDispDoc;
+			if (spHTMLDoc != NULL) {
+				IHTMLWindow2 *spWindow;
+				if (SUCCEEDED(spHTMLDoc->get_parentWindow(&spWindow))) {
+					VARIANT vEmpty = {0};
+					CComBSTR js_code = L"alert(222)";
+					CComBSTR bstrLanguage = L"javascript";
+					spWindow->execScript(js_code, bstrLanguage, &vEmpty);
+				}
+			}
+		}
+	} 
+}
--- SevenFanli/SevenFanliBHO.h
+++ SevenFanli/SevenFanliBHO.h
@@ -6,7 +6,8 @@
 
 
 #include "SevenFanli_i.h"
-
+#include <shlguid.h>  
+#include <exdispid.h>  
 
 
 #if defined(_WIN32_WCE) && !defined(_CE_DCOM) && !defined(_CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA)
@@ -22,12 +23,11 @@ class ATL_NO_VTABLE CSevenFanliBHO :
 	public CComObjectRootEx<CComSingleThreadModel>,
 	public CComCoClass<CSevenFanliBHO, &CLSID_SevenFanliBHO>,
 	public IObjectWithSiteImpl<CSevenFanliBHO>,
-	public IDispatchImpl<ISevenFanliBHO, &IID_ISevenFanliBHO, &LIBID_SevenFanliLib, /*wMajor =*/ 1, /*wMinor =*/ 0>
+	public IDispatchImpl<ISevenFanliBHO, &IID_ISevenFanliBHO, &LIBID_SevenFanliLib, /*wMajor =*/ 1, /*wMinor =*/ 0>,
+	public IDispEventImpl<1, CSevenFanliBHO, &DIID_DWebBrowserEvents2, &LIBID_SHDocVw, 1, 1>  
 {
 public:
-	CSevenFanliBHO()
-	{
-	}
+	CSevenFanliBHO();
 
 DECLARE_REGISTRY_RESOURCEID(IDR_SEVENFANLIBHO)
 
@@ -53,9 +53,26 @@ END_COM_MAP()
 	}
 
 public:
+	STDMETHOD(SetSite)(IUnknown *pUnkSite);  
+	BEGIN_SINK_MAP(CSevenFanliBHO)  
+		SINK_ENTRY_EX(1, DIID_DWebBrowserEvents2, DISPID_BEFORENAVIGATE2, OnBeforeNavigate2)
+		SINK_ENTRY_EX(1, DIID_DWebBrowserEvents2, DISPID_DOCUMENTCOMPLETE, OnDocumentComplete) 
+		SINK_ENTRY_EX(1, DIID_DWebBrowserEvents2, DISPID_DOWNLOADCOMPLETE, OnDownloadComplete)
+	END_SINK_MAP()  
 
-
-
+	// DWebBrowserEvents2  
+	void STDMETHODCALLTYPE OnBeforeNavigate2(IDispatch* pDisp, VARIANT* URL, 
+		VARIANT* pvtFlags,
+		VARIANT* pvtTargetFrameName, 
+		VARIANT* pvtPostData, 
+		VARIANT* pvtHeaders,
+		VARIANT_BOOL* pvbCancel);
+	void STDMETHODCALLTYPE OnDocumentComplete(IDispatch *pDisp, VARIANT *pvarURL);  
+	void STDMETHODCALLTYPE OnDownloadComplete();
+private:  
+	CComPtr<IWebBrowser2>  m_spWebBrowser;  
+	BOOL m_fAdvised;  
+	BOOL m_isRefresh;
 };
 
 OBJECT_ENTRY_AUTO(__uuidof(SevenFanliBHO), CSevenFanliBHO)
