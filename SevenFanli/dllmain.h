882e8eaae1382834911b16290892aa653ba61798
weijiazhen <wjz2047@163.com>
2017-11-01 11:56:35 +0800

    创建ATL工程

---
+++ SevenFanli/dllmain.h
@@ -0,0 +1,10 @@
+// dllmain.h : 模块类的声明。
+
+class CSevenFanliModule : public ATL::CAtlDllModuleT< CSevenFanliModule >
+{
+public :
+	DECLARE_LIBID(LIBID_SevenFanliLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_SEVENFANLI, "{0DEE8D0C-1812-4D38-97B5-750559225FEA}")
+};
+
+extern class CSevenFanliModule _AtlModule;
